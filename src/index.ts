46f4e6b376378b5d18784c0845ff6036b55e6ed9
ThanhNhanNguyen <thanhnhan.b12002@gmail.com>
2022-05-16 15:04:29 +1000

    add getHomepageAPI and getTrainningSessionsAPI functions

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,7 @@ const port = process.env.SD_SERVER_PORT || 3000;
 const DBtoken = process.env.SD_SERVER_INFLUX_API_KEY || 'SKCqeTd4N-0fYfMPo37Ro8Pv_d-PQX4SoEpfYMTyCdV2Ucjif9RNy-5obta8cQRqKlpB25YvOKkT4tdqxw__Gg==';  
 const url ='https://ap-southeast-2-1.aws.cloud2.influxdata.com';
 const client = new InfluxDB({ url: url, token: DBtoken });
+const DEFAULT_PLAYER = 'Warren'
 
 
 let org = 'qethanmoore@gmail.com';
@@ -50,6 +51,7 @@ declare module 'express-session' {
   }
 }
 
+/*
 app.get('/profile', async (req, res) => {
   const playerUsername = 'Warren';
   //if the username is not provided, then username is Warren
@@ -174,14 +176,124 @@ app.get('/profile/:username', async (req, res) => {
 
   res.send(homepageInfo)
 });
+*/
+
+app.get('/profile', async (req, res) => {
+  // username should be set to the username in the session variable if it is not provided. 
+  ///Currently, the default users that will be returned is Warren
+  let username  = 'Warren' ;
+  let homepageAPI = await getHomepageAPI(username);
+  res.send(homepageAPI);
+});
+
+app.get('/profile/:username', async (req, res) => {
+  let username  = req.params.username;
+  let homepageAPI = await getHomepageAPI(username);
+  res.send(homepageAPI);
+});
+
+app.get('/sessions/:username', async (req, res) => {
+  let username  = req.params.username;
+  let trainningSessionsAPI = await getTrainingSessionsAPI(username);
+  res.send(trainningSessionsAPI);
+});
+
+app.get('/sessions', async (req, res) => {
+  let username  = DEFAULT_PLAYER;
+  let trainningSessionsAPI = await getTrainingSessionsAPI(username);
+  res.send(trainningSessionsAPI);
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
 
+async function getHomepageAPI(username: string) {
+  let PLAYER = username;
+  console.log("PLAYER: "+PLAYER);
+
+  let queryPlayersSessions  = readFileSync(resolve(__dirname, '../../queries/players_sessions.flux'), { encoding: 'utf8' });
+  queryPlayersSessions = interpole(queryPlayersSessions, [PLAYER]);
+  
+  let queryPlayersTeams = readFileSync(resolve(__dirname, '../../queries/players_teams.flux'), { encoding: 'utf8' });
+  queryPlayersTeams = interpole(queryPlayersTeams, [PLAYER]);   
+   
+  //get the information of all the training sessions of given players
+  const trainingSessions = await executeInflux(queryPlayersSessions, queryClient);
+  const cleanedTrainingSessions:any[] = []
+  for(let i = 0; i<trainingSessions.length; i++ )
+  {
+    const session = {
+      "playerName": "",
+      "sessionName": "",
+      "sessionDate": "",
+      "sessionTime": "",
+      "teamName": "",
+    } as SessionResponseType;
+    session["playerName"] = trainingSessions[i]["Player Name"]
+    session["sessionName"] = trainingSessions[i]["Session"].split(" ")[0]
+    session["sessionDate"] = moment(trainingSessions[i]["_time"]).format("DD-MMM-YYYY")
+    session["sessionTime"] = moment(trainingSessions[i]["_time"]).format("HH:MM")
+    session["teamName"] = trainingSessions[i]["_measurement"]
+    cleanedTrainingSessions.push(session)
+  }
+
+  //get the teams that the givne player joined in
+  const teams =  await executeInflux(queryPlayersTeams, queryClient);
+  const cleanedTeams:string[] = []
+  for(let i = 0; i < teams.length; i++ )
+  {
+    cleanedTeams.push(teams[i]["_measurement"])
+  }
+
+  //define the structure of the API that will be returned to fronend
+  const homepageInfo = {
+    "name": "",
+    "email": "",
+    "dob": '',
+    'nationality':'',
+    'height':0,
+    'weight':0,
+    'teams':['', '' ],
+    'trainingSessions':[{}, {}],
+  }
+  homepageInfo['name'] = PLAYER 
+  homepageInfo['teams'] = cleanedTeams
+  homepageInfo['trainingSessions'] = cleanedTrainingSessions
+
+  return homepageInfo
+}
+
+async function getTrainingSessionsAPI(username: string) {
+  //get the information of all the training sessions of given players
+  let PLAYER = username;
+  let queryPlayersSessions  = readFileSync(resolve(__dirname, '../../queries/players_sessions.flux'), { encoding: 'utf8' });
+  queryPlayersSessions = interpole(queryPlayersSessions, [PLAYER]);
+
+  const trainingSessions = await executeInflux(queryPlayersSessions, queryClient);
+  const cleanedTrainingSessions:any[] = [];
+  for (let i = 0; i<trainingSessions.length; i++ )
+  {
+    const session = {
+      "playerName": "",
+      "sessionName": "",
+      "sessionDate": "",
+      "sessionTime": "",
+      "teamName": "",
+    } as SessionResponseType;
+    session["playerName"] = trainingSessions[i]["Player Name"]
+    session["sessionName"] = trainingSessions[i]["Session"].split(" ")[0]
+    session["sessionDate"] = moment(trainingSessions[i]["_time"]).format("DD-MMM-YYYY")
+    session["sessionTime"] = moment(trainingSessions[i]["_time"]).format("HH:MM")
+    session["teamName"] = trainingSessions[i]["_measurement"]
+    cleanedTrainingSessions.push(session);
+  }  
+  return cleanedTrainingSessions;
+}
+
 //function to run the InfluxDB querry
 const executeInflux = async (fluxQuery: string, queryClient: QueryApi ) => {
-  let sessionArray : any[] = [] 
+  let sessionArray : any[] = []; 
 
   await new Promise<void>((resolve, reject) => {
     let rejected = false;
